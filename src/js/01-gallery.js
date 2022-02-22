00feb190b6ebf6414c36a45c7f0b9a37a7dc87d4
Nataliia Khintsitska <Natalienumenna@gmail.com>
2022-02-22 22:32:30 +0200

    first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+
+import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+const imagesContainer = document.querySelector('.gallery');
+const cardsMarkup = createImageCardsMarkup(galleryItems);
+imagesContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+imagesContainer.addEventListener('click', onImagesContainerClick);
+
+function createImageCardsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `      
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+      `
+    }).join('');
+}
+
+function onImagesContainerClick(e) {
+    const isGalleryItemEl = e.target.classList.contains('gallery__image');
+    const imageSrc = e.target.dataset.source;
+    e.preventDefault();
+    if (!isGalleryItemEl) {
+        return;
+    }
+}
+let lightbox = new SimpleLightbox('.gallery__item', {captionsData: 'alt', captionDelay: 250, });
+    
+    console.log(galleryItems);
